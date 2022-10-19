54be471a8997d88a1a41638bd7a34463065644e1
n4sky1 <n4sky@yandex.ru>
2022-10-19 11:31:34 +0700

    This is a new commit for what I originally planned to be amended

---
+++ precache-manifest.f69aa37e8376aea3644deb31dd92839e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3932346a265f01c9d654415d66279463",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14285d28d63e09acad60",
+    "url": "/static/css/2.b550b4be.chunk.css"
+  },
+  {
+    "revision": "ae1e56c2e1edea12503b",
+    "url": "/static/css/main.1735637a.chunk.css"
+  },
+  {
+    "revision": "14285d28d63e09acad60",
+    "url": "/static/js/2.95ffd267.chunk.js"
+  },
+  {
+    "revision": "ae1e56c2e1edea12503b",
+    "url": "/static/js/main.c96f40cb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "abb8a771e188ec9fd23ba3ef323d6f24",
+    "url": "/static/media/close.abb8a771.svg"
+  }
+]);
